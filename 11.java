c4704c59f6757b17ca6626d7297d4690a29ff4ee
Han J W <30855535+NjustJiaweihan@users.noreply.github.com>
2019-03-22 10:54:22 +0800

    Create 11.java

---
+++ 11.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        //对于多个测试用例写个while循环
+        while (scanner.hasNext())
+        {
+            char[] chars = (scanner.nextInt() + "").toCharArray();
+            StringBuilder s = new StringBuilder();
+            for(int i=chars.length-1; i>=0; i--)
+            {
+                s.append(chars[i]);
+            }
+            System.out.println(s);
+
+        }
+        scanner.close();
+    }
+}
