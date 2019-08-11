6e291cd2f264ccfea07230167a5bdc38424c879e
Han J W <30855535+NjustJiaweihan@users.noreply.github.com>
2019-08-11 21:33:37 +0800

    Create Main4.java

---
+++ 2019-08-11-字节跳动/Main4.java
@@ -0,0 +1,61 @@
+package cn.deepblog.ByteDance;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main4 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int V = scanner.nextInt();
+
+        List<List<Integer>> adj = new ArrayList<List<Integer>>(V+1);
+
+        for(int i=0; i<=V; i++){
+
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<V-1; i++){
+
+            int var1 = scanner.nextInt();
+            int var2 = scanner.nextInt();
+
+            adj.get(var1).add(var2);
+            adj.get(var2).add(var1);
+        }
+
+        boolean[] marked = new boolean[V+1];
+
+        int source = 1;
+        List<Integer> lens = new ArrayList<>();
+
+        int cnt = 0;
+        marked[source] = true;
+        for(int w : adj.get(source)){
+
+            dfs(w, adj, marked, cnt, lens);
+        }
+
+        System.out.println(lens);
+
+    }
+
+    private static void dfs(int v, List<List<Integer>> adj, boolean[] marked, int cnt, List<Integer> lens){
+
+        cnt++;
+        marked[v] = true;
+
+        for(int w : adj.get(v)){
+
+            if(!marked[w]){
+                dfs(w, adj, marked, cnt, lens);
+            }
+        }
+
+        lens.add(cnt);
+    }
+}
